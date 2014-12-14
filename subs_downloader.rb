0adf9ab0614316c7bdbe71dce45f2039b7aec537
gautamsawhney <gsawhney21@gmail.com>
2014-12-14 10:44:46 +0530

    Checking if the file is a video format or not

--- subs_downloader.rb
+++ subs_downloader.rb
@@ -1,5 +1,5 @@
-
 require 'digest/md5'
+
 def get_hash(name)
 	readsize = 64 * 1024
 	File.open(name, 'rb') do |f|
@@ -10,4 +10,27 @@ def get_hash(name)
 		return Digest::MD5.hexdigest(data)
   end
 end
+
+
+def subs_downloader(filename)
+	vid_extensions = [".avi",".mp4",".mkv",".mpg",".mpeg",".mov",".rm",".vob",".wmv",".flv",".3gp"]
+  begin
+		orignal_filename = filename
+		vid_extensions.each do |vix|
+			filename = filename.gsub(vix , "")
+		end
+
+		if orignal_filename == filename
+			return
+		end
+
+		puts "hello kitty"	
+
+	rescue Exception => e
+	
+  end	
+
+end
+
+subs_downloader("abc.avi")
 
